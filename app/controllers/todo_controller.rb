17ebb5b9b16254e3a47586fe94609f6ee9f2c67c
Mykyah Green <mykyah.green@epicacademy.org>
2017-10-22 15:21:10 +0000

    Remove conditional logic and pull date straight from the distance

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,15 +3,6 @@ class TodoController < ApplicationController
     end
     
     def show
-        todo_id = params[:id]
-        
-        if todo_id == '1'
-            @todo_description = "make the curriculum"
-            @todo_pomodora_estimate = 4
-            
-        elsif todo_id == '2'
-            @todo_description = "walk the dog"
-            @todo_pomodora_estimate = 2
-        end
+        @todo = Todo.find_by_id(params[:id])
     end
 end
