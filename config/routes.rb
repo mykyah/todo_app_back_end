3bcd7967ff576709bd9055443b9048dad030821f
Mykyah Green <mykyah.green@epicacademy.org>
2017-10-22 18:10:59 +0000

    Add the edit update and destroy RCAV flows

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/edit/:id' , to: 'todo#edit'
+  get 'todo/update/:id' , to: 'todo#update'
+  get 'todo/destroy/:id', to: 'todo#destroy'
   get 'todo/show/:id', to: 'todo#show'
 end
 
