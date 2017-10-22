65a1564bc792c3746875e2a6f2b9a5af075bdb55
Mykyah Green <mykyah.green@epicacademy.org>
2017-10-22 19:13:16 +0000

    Add root route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,6 +7,17 @@ class TodoController < ApplicationController
         @todo = Todo.find_by_id(params[:id])
     end
     
+    def new
+    end
+    
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    
     def edit
         @todo = Todo.find_by_id(params[:id])
     end
